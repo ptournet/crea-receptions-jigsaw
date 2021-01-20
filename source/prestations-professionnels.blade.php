f5716a055ed82ca023b2953c37dd60ba016fbab0
Patrick Tournet <ptournet@users.noreply.github.com>
2021-01-20 07:39:26 +0100

    Finalisation prestas pros

--- source/prestations-professionnels.blade.php
+++ source/prestations-professionnels.blade.php
@@ -7,4 +7,139 @@ title_img: /assets/img/main-professionnelles.png
 @extends('_layouts.page')
 
 @section('pagecontent')
+<div class="relative mx-auto mb-20 max-w-929">
+    <div class="z-20 w-4/5 pb-10 grey-background top-24 h-480 -right-24"></div>
+    <div class="relative z-50 mt-80px">
+        <div class="w-full overflow-hidden">
+            <img class="max-w-none" src="{{ $page->baseUrl }}/assets/img/content/3_IMG_960x386.png" alt="">
+            <div class="relative right-0 float-right pb-6 text-sm leading-6 w-55prct font-base mt-7">
+                Inauguration, séminaire, pot de départ, pour vos clients ou vos collaborateurs, nous sommes là pour répondre à vos attentes, tant dans l’élaboration des menus que dans l’organisation de votre réception.
+                <div class="flex space-x-8 text-base mt-18">
+                    <div class="py-1 bg-gray-800 px-7">
+                        <a href="{{ $page->baseUrl }}/contact/">
+                            <div class="font-sans font-bold text-white">
+                                réaliser un
+                                <span class="underline font-script text-anis">devis</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="absolute bottom-24 green-bloc3 w-300 h-300 left-16 pt-45px"> <!--</div> pt-4 pb-12 pr-10 -top-60px -left-30px w-300 h-480 green-bloc pl-9">-->
+            <div class="relative mx-auto text-3xl text-center text-white w-213 h-201 green-title pt-55px">
+                <p class="m-0 font-sans">cocktails</p>
+                <p class="mt-0 font-script">et menus</p>
+                <div class="absolute h-1.4 bg-gray-900 w-15 top-95px -right-80px"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="relative mx-auto mb-20 max-w-929">
+    <div class="z-20 w-4/5 pb-10 grey-background top-24 h-480 -left-24"></div>
+    <div class="relative z-50 mt-80px">
+        <div class="w-full overflow-hidden">
+            <img class="max-w-none" src="{{ $page->baseUrl }}/assets/img/content/2_IMG_960x386.png" alt="">
+            <div class="relative right-0 float-left pb-6 text-sm leading-6 w-55prct font-base mt-7">
+                Viennoiseries pur beurre, jus de fruits, sucre, café Nespresso et thé. Pour démarrer au mieux votre journée de travail, nous vous proposons différentes formules : petit déjeuner, pause-café ou pause-sucrée...
+                <div class="flex space-x-8 text-base mt-18">
+                    <div class="py-1 bg-gray-800 px-7">
+                        <a href="{{ $page->baseUrl }}/contact/">
+                            <div class="font-sans font-bold text-white">
+                                réaliser un
+                                <span class="underline font-script text-anis">devis</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="absolute bottom-24 green-bloc3 w-300 h-300 right-16 pt-45px"> <!--</div> pt-4 pb-12 pr-10 -top-60px -left-30px w-300 h-480 green-bloc pl-9">-->
+            <div class="relative mx-auto text-3xl text-center text-white w-213 h-201 green-title pt-55px">
+                <p class="m-0 font-sans">petits déjeuners</p>
+                <p class="mt-0 font-script">pauses sucrées</p>
+                <div class="absolute h-1.4 bg-gray-900 w-15 top-95px -left-80px"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="relative mx-auto mb-20 max-w-929">
+    <div class="z-20 w-4/5 pb-10 grey-background top-24 h-480 -right-24"></div>
+    <div class="relative z-50 mt-80px">
+        <div class="w-full overflow-hidden">
+            <img class="max-w-none" src="{{ $page->baseUrl }}/assets/img/content/6_IMG_960x386.png" alt="">
+            <div class="relative right-0 float-right pb-6 text-sm leading-6 w-55prct font-base mt-7">
+                Une solution rapide et équilibrée. Pour vos déjeuners d’affaire ou vos réunions entre collaborateurs, nous proposons 5 sortes de plateaux dont un végétarien.
+                <div class="flex space-x-8 text-base mt-18">
+                    <div class="py-1 bg-gray-800 px-7">
+                        <a href="{{ $page->baseUrl }}/contact/">
+                            <div class="font-sans font-bold text-white">
+                                réaliser un
+                                <span class="underline font-script text-anis">devis</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="absolute bottom-24 green-bloc3 w-300 h-300 left-16 pt-45px"> <!--</div> pt-4 pb-12 pr-10 -top-60px -left-30px w-300 h-480 green-bloc pl-9">-->
+            <div class="relative mx-auto text-3xl text-center text-white w-213 h-201 green-title pt-55px">
+                <p class="m-0 font-sans">plateaux</p>
+                <p class="mt-0 font-script">repas</p>
+                <div class="absolute h-1.4 bg-gray-900 w-15 top-95px -right-80px"></div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="relative mx-auto mb-36 max-w-929">
+    <div class="relative z-50 flex flex-wrap">
+        <div class="w-900">
+            <div class="relative mx-auto h-300 w-300 grey-bloc pt-45px">
+                <div class="relative mx-auto text-3xl text-center text-gray-900 w-213 h-201 pt-55px grey-title">
+                    <p class="m-0 font-sans">dernières</p>
+                    <p class="mt-0 font-script">références</p>
+                    <div class="absolute z-50 h-20 w-1.4 bg-anis left-100 -top-24 m-auto"></div>
+                </div>
+            </div>
+        </div>
+        <div class="relative overflow-hidden h-300 w-300">
+            <a class="fancybox" rel="group" href="{{ $page->baseUrl }}/assets/img/content/Pro-3.jpg" title="Buffet événement professionnel">
+                <img class="ref_img" src="{{ $page->baseUrl }}/assets/img/content/Pro-3.jpg">
+            </a>
+        </div>
+        <div class="relative overflow-hidden h-300 w-300">
+            <a class="fancybox" rel="group" href="{{ $page->baseUrl }}/assets/img/content/Pro-2.jpg" title="Buffet petit déjeuner">
+                <img class="ref_img" src="{{ $page->baseUrl }}/assets/img/content/Pro-2.jpg">
+            </a>
+        </div>
+        <div class="relative overflow-hidden h-300 w-300">
+            <a class="fancybox" rel="group" href="{{ $page->baseUrl }}/assets/img/content/Inga-riaux-2.jpg" title="Cocktail sur l'Inga des Riaux">
+                <img class="ref_img" src="{{ $page->baseUrl }}/assets/img/content/Inga-riaux-2.jpg">
+            </a>
+        </div>
+        <div class="relative overflow-hidden h-300 w-300">
+            <a class="fancybox" rel="group" href="{{ $page->baseUrl }}/assets/img/content/Inga-riaux-1.jpg" title="Cocktail sur l'Inga des Riaux - Table">
+                <img class="ref_img" src="{{ $page->baseUrl }}/assets/img/content/Inga-riaux-1.jpg">
+            </a>
+        </div>
+        <div class="relative overflow-hidden h-300 w-300">
+            <a class="fancybox" rel="group" href="{{ $page->baseUrl }}/assets/img/content/Pro-1.jpg" title="Cocktail séminaire">
+                <img class="ref_img" src="{{ $page->baseUrl }}/assets/img/content/Pro-1.jpg">
+            </a>
+        </div>
+        <div class="relative overflow-hidden h-300 w-300">
+            <a class="fancybox" rel="group" href="{{ $page->baseUrl }}/assets/img/content/cocktail-1.jpg" title="Cocktail salé - 1">
+                <img class="ref_img" src="{{ $page->baseUrl }}/assets/img/content/cocktail-1.jpg">
+            </a>
+        </div>
+        <div class="w-900">
+            <a href="{{ $page->baseUrl }}/references/">
+                <div class="box-content absolute left-0 right-0 py-2 mx-auto font-sans text-white px-7 -bottom-20 bg-anis w-177">
+                    voir d'autres <span class="font-bold text-gray-800 underline font-script">références</span>
+                </div>
+            </a>
+        </div>
+    </div>
+    <div class="absolute z-20 h-full pb-24 bg-gray-700 w-200prct -left-1/2 top-36"></div>
+</div>
 @endsection
