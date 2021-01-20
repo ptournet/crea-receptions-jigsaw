a855e9e64ca648fe16d65e9fcdf0e7bb09c5533c
Patrick Tournet <ptournet@users.noreply.github.com>
2021-01-20 10:02:25 +0100

    Start partenaires

--- source/partenaires.blade.php
+++ source/partenaires.blade.php
@@ -7,4 +7,10 @@ title_img: /assets/img/main-partenaires.png
 @extends('_layouts.page')
 
 @section('pagecontent')
+    <div class="-ml-8 font-sans font-bold text-center text-gray-100 text-16xl">particuliers</div>
+    <div>
+    </div>
+    <div class="-ml-8 font-sans font-bold text-center text-gray-100 text-16xl">professionnels</div>
+    <div>
+    </div>
 @endsection
