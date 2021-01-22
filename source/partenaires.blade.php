05975f698374cf92c611f4e7a0b40560c88daa55
Patrick Tournet <ptournet@users.noreply.github.com>
2021-01-22 22:38:05 +0100

    Partenaires all images and links

--- source/partenaires.blade.php
+++ source/partenaires.blade.php
@@ -25,13 +25,20 @@ title_img: /assets/img/main-partenaires.png
                 </a>
             </div>
             <div class="relative">
+                <div>
+                    <img class="relative max-w-772 max-h-463" src="{{ $page->baseUrl }}/assets/img/content/1431077523.jpg" alt="Chateau de la Beaumetane">
+                </div>
                 <div></div>
-                <div></div>
-                <a href="" target="_blank"></a>
+                <a href="https://www.chateau-la-beaumetane.fr/en/home/" target="_blank">
+                    <div class="absolute py-2 font-sans text-white bg-gray-800 -bottom-5 px-7 left-16">
+                        découvrir ce
+                        <span class="font-bold underline text-anis font-script">partenaire</span>
+                    </div>
+                </a>
             </div>
             <div class="relative">
                 <div>
-                    <img class="relative left-12 max-w-772 max-h-463" src="{{ $page->baseUrl }}/assets/img/content/780x480.jpg" alt="Chateau de la Buzine">
+                    <img class="relative left-16 max-w-772 max-h-463" src="{{ $page->baseUrl }}/assets/img/content/780x480.jpg" alt="Chateau de la Buzine">
                 </div>
                 <div></div>
                 <a href="https://labuzine.com/fr" target="_blank">
@@ -43,9 +50,15 @@ title_img: /assets/img/main-partenaires.png
             </div>
             <div class="relative">
                 <div>
+                    <img class="relative max-w-772 max-h-463" src="{{ $page->baseUrl }}/assets/img/content/S135876182A.jpg" alt="Le Mas des Garguiers">
                 </div>
                 <div></div>
-                <a href="" target="_blank"></a>
+                <a href="http://www.mas-garguier.fr/" target="_blank">
+                    <div class="absolute py-2 font-sans text-white bg-gray-800 -bottom-5 px-7 left-16">
+                        découvrir ce
+                        <span class="font-bold underline text-anis font-script">partenaire</span>
+                    </div>
+                </a>
             </div>
             <div class="relative">
                 <div>
@@ -60,13 +73,20 @@ title_img: /assets/img/main-partenaires.png
                 </a>
             </div>
             <div class="relative">
+                <div>
+                    <img class="relative max-w-772 max-h-463" src="{{ $page->baseUrl }}/assets/img/content/grand_callamand.jpg" alt="Chateau Grand Callamand">
+                </div>
                 <div></div>
-                <div></div>
-                <a href="" target="_blank"></a>
+                <a href="http://www.chateaugrandcallamand.com/" target="_blank">
+                    <div class="absolute py-2 font-sans text-white bg-gray-800 -bottom-5 px-7 left-16">
+                        découvrir ce
+                        <span class="font-bold underline text-anis font-script">partenaire</span>
+                    </div>
+                </a>
             </div>
             <div class="relative">
                 <div>
-                    <img class="relative left-12 max-w-772 max-h-463" src="{{ $page->baseUrl }}/assets/img/content/chateau de la dorgonne.jpg" alt="Domaine de la Siberie">
+                    <img class="relative left-20 max-w-772 max-h-463" src="{{ $page->baseUrl }}/assets/img/content/chateau de la dorgonne.jpg" alt="Domaine de la Siberie">
                 </div>
                 <div></div>
                 <a href="https://www.chateauladorgonne.com/" target="_blank">
@@ -79,7 +99,46 @@ title_img: /assets/img/main-partenaires.png
         </div>
     </div>
     <div class="-ml-8 font-sans font-bold text-center text-gray-100 text-16xl">professionnels</div>
-    <div>
+    <div class="relative -top-9">
+        <div class="z-10 w-3/5 grey-background h-93prct mb-50px top-20"></div>
+        <div class="relative z-50 mx-auto space-y-36 max-w-929">
+            <div class="relative">
+                <div>
+                    <img class="relative left-12 max-w-772 max-h-463" src="{{ $page->baseUrl }}/assets/img/content/agora.jpg" alt="Palais des congrès Agora">
+                </div>
+                <div></div>
+                <a href="http://www.agora-congres.com/" target="_blank">
+                    <div class="absolute py-2 font-sans text-white bg-gray-800 -bottom-5 px-7 right-44">
+                        découvrir ce
+                        <span class="font-bold underline text-anis font-script">partenaire</span>
+                    </div>
+                </a>
+            </div>
+            <div class="relative">
+                <div>
+                    <img class="relative max-w-772 max-h-463" src="{{ $page->baseUrl }}/assets/img/content/18222636_1602543539756243_6380681393446553017_n.jpg" alt="Le BUZ">
+                </div>
+                <div></div>
+                <a href="http://www.buz-marseille.fr/" target="_blank">
+                    <div class="absolute py-2 font-sans text-white bg-gray-800 -bottom-5 px-7 left-16">
+                        découvrir ce
+                        <span class="font-bold underline text-anis font-script">partenaire</span>
+                    </div>
+                </a>
+            </div>
+            <div class="relative">
+                <div>
+                    <img class="relative left-48 max-w-772 max-h-463" src="{{ $page->baseUrl }}/assets/img/content/le-moulin-de-gemenos-mariage-pre-13.jpg" alt="Le Moulin de Gemenos">
+                </div>
+                <div></div>
+                <a href="http://www.moulin-gemenos.fr/" target="_blank">
+                    <div class="absolute py-2 font-sans text-white bg-gray-800 -bottom-5 px-7 right-44">
+                        découvrir ce
+                        <span class="font-bold underline text-anis font-script">partenaire</span>
+                    </div>
+                </a>
+            </div>
+        </div>
     </div>
 </div>
 @endsection
